a04ae51fcba7b1a8a6e9b71fcb25c08db3ea2511
Mof <mof@labf.org>
2023-07-13 15:49:34 +0930

    Initial commit

---
+++ TestNavBarScrolling/ViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  TestNavBarScrolling
+//
+//  Created by Mof on 13/7/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+  @IBOutlet weak var tableView: UITableView!
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+  func numberOfSections(in tableView: UITableView) -> Int {
+      // #warning Incomplete implementation, return the number of sections
+      return 1
+  }
+
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+      // #warning Incomplete implementation, return the number of rows
+      return 100
+  }
+
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+      let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+
+    cell.textLabel?.text = "Cell \(indexPath.row)"
+
+      return cell
+  }
+
+}
+
